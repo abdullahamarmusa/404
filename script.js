6cf89d5c21506622fb7425bdd9b25d2e41efd108
Abdullah Amar Musa <105099811+abdullahamarmusa@users.noreply.github.com>
2024-02-22 21:22:34 +0600

    Little Changes
    some changes in title and script
    
    Co-Authored-By: John Millar <139971121+johnmillariu@users.noreply.github.com>

--- script.js
+++ script.js
@@ -17,3 +17,16 @@ anime({
   scale: [{value: 1}, {value: 1.4}, {value: 1, delay: 250}],
     rotateY: {value: '+=180', delay: 200},
 });
+
+
+
+
+// Path: script.js
+anime({
+  targets: '.row svg',
+  translateY: 10,
+  autoplay: true,
+  loop: true,
+  easing: 'easeInOutSine',
+  direction: 'alternate'
+});
